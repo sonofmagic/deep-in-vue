fb8c3cd00f22fcdf4f201bf30c1e5e8d2460c951
ice breaker <1324318532@qq.com>
2025-04-14 15:37:45 +0800

    chore: withDirectives

--- apps/admin/src/layouts/components/index.ts
+++ apps/admin/src/layouts/components/index.ts
@@ -1,14 +1,46 @@
-import { ElMenu, ElMenuItem } from 'element-plus'
-import { defineComponent, h } from 'vue'
+import { ElCheckbox, ElMenu, ElMenuItem, ElRadioButton, ElRadioGroup } from 'element-plus'
+import { defineComponent, h, reactive, vShow, withDirectives } from 'vue'
+
+const sharedState = reactive({
+  isCollapse: true,
+  isShown: false,
+})
 
 export const BaseHeader = defineComponent({
   name: 'BaseHeader',
   setup() {
     return () => {
       return h('div', {
-        class: 'h-16 border-b border-(--el-menu-border-color)',
+        class: 'h-16 border-b border-(--el-menu-border-color) flex items-center justify-between px-4',
       }, [
-        'ice-admin',
+        h('div', [
+          'ice-admin',
+
+        ]),
+        h('div', {
+          class: 'flex items-center space-x-5',
+        }, [
+          h('div', [
+            h(ElCheckbox, {
+              'modelValue': sharedState.isShown,
+              'onUpdate:modelValue': val => sharedState.isShown = val as boolean,
+            }, [
+              'isShown',
+            ]),
+          ]),
+
+          h('div', { class: 'shrink-0' }, [
+            h(
+              ElRadioGroup,
+              { 'modelValue': sharedState.isCollapse, 'onUpdate:modelValue': val => sharedState.isCollapse = val as boolean },
+              [
+                h(ElRadioButton, { value: true }, () => 'expand'),
+                h(ElRadioButton, { value: false }, () => 'collapse'),
+              ],
+            ),
+          ]),
+
+        ]),
       ])
     }
   },
@@ -18,19 +50,43 @@ export const BaseSidebar = defineComponent({
   name: 'BaseSidebar',
   setup() {
     return () => {
+      const menus = [
+        {
+          label: 'home',
+          index: '/',
+        },
+        {
+          label: 'dashborad',
+          index: 'dashborad',
+        },
+      ]
+
       return h('div', {
-        class: 'w-64',
+
       }, [
         h(
           ElMenu,
           {
             router: true,
-            class: 'h-full',
+            class: 'h-full w-64',
+            collapse: sharedState.isCollapse,
           },
-          [
-            h(ElMenuItem, { index: '/' }, () => 'home'),
-            h(ElMenuItem, { index: 'dashborad' }, () => 'dashborad'),
-          ],
+          () => {
+            return [
+              menus.map((x) => {
+                return h(ElMenuItem, { index: x.index }, () => x.label)
+              }),
+              withDirectives(
+                h(ElMenuItem, [
+                  'isShown',
+                ]),
+                [
+                  [vShow, sharedState.isShown],
+                ],
+              ),
+            ]
+          },
+
         ),
       ])
     }
