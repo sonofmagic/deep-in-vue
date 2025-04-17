7c2369407fd15d06c4985df21c128fbed8177342
ice breaker <1324318532@qq.com>
2025-04-17 15:53:00 +0800

    chore: patch

--- apps/fully-compiled/src/pages/index.vue
+++ apps/fully-compiled/src/pages/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import HelloWorld, { Happy } from '@/components/HelloWorld.vue'
+import HelloWorld, { Happy, Sad } from '@/components/HelloWorld.vue'
 </script>
 
 <template>
   <div class="mx-auto container">
     <HelloWorld />
     <Happy />
+    <Sad />
   </div>
 </template>
